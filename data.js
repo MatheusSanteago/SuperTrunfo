02346298286d43b73a84e4eab47e841a833af4ab
Matheus <matheussanteago@gmail.com>
2022-03-15 19:32:08 -0300

    finalizando

--- data.js
+++ data.js
@@ -1,16 +1,20 @@
 var card1 = {nome:"Messi",url: "https://sortitoutsi.net/uploads/face/7458500.png",atributes: {Drible: 95,Defesa: 34,Chute: 92,}};
 var card2 = {nome:"Cristiano Ronaldo", url:"https://sortitoutsi.net/uploads/face/735216.png",atributes: {Drible: 88,Defesa: 34,Chute: 93,}};
 var card3 = {nome:"Neymar",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 94,Defesa: 37,Chute: 83,}};  
-var card4 = {nome:"Vinicius Jr",url:"https://sortitoutsi.net/uploads/face/19302146.png",atributes: {Drible: 87,Defesa: 29,Chute:70,}};  
-var card5 = {nome:"Lewandowski",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 94,Defesa: 37,Chute: 83,}};     
-var card6 = {nome:"De Bruyne",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 88,Defesa: 64,Chute: 86,}};  
-var card7 = {nome:"Mbappé",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 92,Defesa: 36,Chute: 88,}};  
-var card8 = {nome:"Van Dijk",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 94,Defesa: 37,Chute: 83,}};  
-var card9 = {nome:"Kanté",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 82,Defesa: 87,Chute: 66,}};  
-var card10 = {nome:"Casemiro",url:"https://sortitoutsi.net/uploads/face/19024412.png",atributes: {Drible: 73,Defesa: 86,Chute: 73,}};  
+var card4 = {nome:"Kimmich",url:"https://sortitoutsi.net/uploads/face/92039023.png",atributes: {Drible: 84,Defesa: 83,Chute:73,}};  
+var card5 = {nome:"Lewandowski",url:"https://sortitoutsi.net/uploads/face/719601.png",atributes: {Drible: 94,Defesa: 37,Chute: 83,}};     
+var card6 = {nome:"De Bruyne",url:"https://sortitoutsi.net/uploads/face/18004457.png",atributes: {Drible: 88,Defesa: 64,Chute: 86,}};  
+var card7 = {nome:"Mbappé",url:"https://sortitoutsi.net/uploads/face/85139014.png",atributes: {Drible: 92,Defesa: 36,Chute: 88,}};  
+var card8 = {nome:"Van Dijk",url:"https://sortitoutsi.net/uploads/face/37024025.png",atributes: {Drible: 64,Defesa: 37,Chute: 83,}};  
+var card9 = {nome:"Kanté",url:"https://sortitoutsi.net/uploads/face/85081880.png",atributes: {Drible: 82,Defesa: 87,Chute: 66,}};  
+var card10 = {nome:"Casemiro",url:"https://sortitoutsi.net/uploads/face/19061518.png",atributes: {Drible: 73,Defesa: 86,Chute: 73,}};  
+var card11 = {nome:"Varane",url:"https://sortitoutsi.net/uploads/face/85075627.png",atributes: {Drible: 65,Defesa: 87,Chute: 49,}}; 
 
-var newCard = JSON.parse(localStorage.getItem('newcards'));
-var cards = [card1,card2,card3];
-cards.push(newCard)
+var newCard = JSON.parse(localStorage.getItem('cards',cards));
+var cards = [card1,card2,card3,card4,card5,card6,card7,card8,card8,card9,card9,card10,card11];
 
-    
+ if (newCard.length > cards.length){
+     cards = newCard;
+ } else {
+    var cards = [card1,card2,card3,card4,card5,card6,card7,card8,card8,card9,card9,card10,card11];
+ }
--- main.js
+++ main.js
@@ -1,89 +1,106 @@
-var imported = document.createElement('script');
-imported.src = 'data.js';
-document.head.appendChild(imported); 
+var imported = document.createElement("script");
+imported.src = "data.js";
+document.head.appendChild(imported);
 
+//cards = JSON.parse(localStorage.getItem('jogadores') || '[]');
+var cardPC;
+var cardPlayer;
 
-    //cards = JSON.parse(localStorage.getItem('jogadores') || '[]');
-    var cardPC;
-    var cardPlayer;
-    
-    function addPlayer() {
-        var nameValue = document.getElementById('name').value;
-        var dribleValue = document.getElementById('drible-input').value;
-        var defesaValue = document.getElementById('defesa-input').value;
-        var chuteValue = document.getElementById('chute-input').value;
-        var urlValue = document.getElementById('url-input').value;
+function addPlayer() {
+  var nameValue = document.getElementById("name").value;
+  var dribleValue = document.getElementById("drible-input").value;
+  var defesaValue = document.getElementById("defesa-input").value;
+  var chuteValue = document.getElementById("chute-input").value;
+  var urlValue = document.getElementById("url-input").value;
 
-        if (nameValue == ""){
-            alert("Digite um valor");
-        } else {
-            let newCard = {
-                nome: nameValue ,
-                url: urlValue,
-                    atributes: {
-                    Drible: dribleValue,
-                    Defesa: defesaValue,
-                    Chute: chuteValue,
-                }
-            }
-            localStorage.setItem("newcards", JSON.stringify(newCard));
-            cards.push(newCard);
-        }
-        
-        console.log(cards);
+  if (nameValue == "") {
+    alert("Digite o nome do jogador");
+  } else if (urlValue == "") {
+    alert("Digite uma URL");
+  } else {
+    let newCard = {
+      nome: nameValue,
+      url: urlValue,
+      atributes: {
+        Drible: dribleValue,
+        Defesa: defesaValue,
+        Chute: chuteValue,
+      },
+    };
+    cards.push(newCard);
+    localStorage.setItem("cards", JSON.stringify(cards));
+    console.log(cards);
+  }
+}
+function drawCard() {
+  if (!cards.length) {
+    alert("Crie um jogador");
+  } else {
+    var n = parseInt(Math.random() * cards.length);
+    cardPC = cards[n];
+    var m = parseInt(Math.random() * cards.length);
+    cardPlayer = cards[m];
+
+    console.log(cardPlayer);
+    console.log(cardPC);
+  }
+  if (cardPlayer == cardPC) {
+    n = parseInt(Math.random() * cards.length);
+    cardPC = cards[n];
+  }
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  showOptions();
+}
+function showOptions() {
+  var options = document.getElementById("opcoes");
+  var opText = "";
+  for (let atributos in cardPlayer.atributes) {
+    opText +=
+      `<input type="radio" name="atributes" value="${atributos}">` + atributos;
+  }
+  options.innerHTML = opText;
+}
+function atributeSelect() {
+  var radioAtrib = document.getElementsByName("atributes");
+  for (let i = 0; i < radioAtrib.length; i++) {
+    if (radioAtrib[i].checked === true) {
+      return radioAtrib[i].value;
     }
-    function drawCard() {
-        if (!cards.length) {
-            alert('Crie um jogador'); 
-        } else {
-            var n = parseInt(Math.random() * cards.length);
-            cardPC = cards[n];
-            var m = parseInt(Math.random() * cards.length);
-            cardPlayer = cards[m];
+  }
+}
+function jogar() {
+  var atribSelected = atributeSelect();
+  var result = document.getElementById("resultado");
 
-            console.log(cardPlayer);
-            console.log(cardPC);
-        } if (cardPlayer == cardPC){
-            n = parseInt(Math.random() * cards.length);
-            cardPC = cards[n];
-        }
-        document.getElementById('btnSortear').disabled = true;
-        document.getElementById('btnJogar').disabled = false;
-        showOptions();
+  if (atribSelected == undefined) {
+    alert("Escolha um atributo");
+    drawCard();
+  } else {
+    var valuePlayer = cardPlayer.atributes[atribSelected];
+    var valuePC = cardPC.atributes[atribSelected];
 
-    }
-    function showOptions() {
-        var options = document.getElementById('opcoes');
-        var opText = '';
-        for (let atributos in cardPlayer.atributes) {
-            opText += `<input type="radio" name="atributes" value="${atributos}">` + atributos;
-        }
-        options.innerHTML = opText;
-    }   
-    function atributeSelect() {
-        var radioAtrib = document.getElementsByName('atributes');
-        for (let i = 0; i < radioAtrib.length; i++) {
-            if (radioAtrib[i].checked === true) {
-                return radioAtrib[i].value;
-            }
-        }
-    }
-    function jogar() {
-        var atribSelected = atributeSelect();
-        var result = document.getElementById('resultado');
+    if (valuePlayer > valuePC) {
+      result.innerHTML = `<h1>Você ganhou do ${cardPC.nome} no atributo de ${atribSelected}</h1><h3>${cardPlayer.nome} VS ${cardPlayer.nome}</h3><img src="${cardPlayer.url}"><img src="${cardPC.url}"><h2>Sua carta era à do ${cardPlayer.nome}</h2>`;
+      result.style.backgroundColor = "#d3bfbf52";
+      result.style.border = "solid 3px white";
+      result.style.borderRadius = "20px";
+      setInterval(function() {result.style.display = "none"},2000);
 
-        var valuePlayer = cardPlayer.atributes[atribSelected];
-        var valuePC = cardPC.atributes[atribSelected];
-        
-        if (valuePlayer > valuePC) {
-            result.innerHTML = `<h1>Você ganhou do ${cardPC.nome}</h1><img src="${cardPlayer.url}"><h2>sua carta era ${cardPlayer.nome}</h2>`;
-            
-        } else if (valuePC > valuePlayer) {
-            result.innerHTML = `<h1>Você perdeu para ${cardPC.nome}</h1><img src="${cardPlayer.url}"><h2>sua carta era ${cardPlayer.nome}</h2>`;
-            
-        } else {
-            result.innerHTML = `<h1>Você empatou com ${cardPC.nome}</h1><img src="${cardPlayer.url}"><h2>sua carta era ${cardPlayer.nome}</h2>`;
-        }
-        //console.log(cardPlayer.atributes[atribSelected]);
-        document.getElementById('btnSortear').disabled = false;
+    } else if (valuePC > valuePlayer) {
+      result.innerHTML = `<h1>Você perdeu para ${cardPC.nome} no atributo de ${atribSelected}</h1><img src="${cardPlayer.url}"><img src="${cardPC.url}"><h2>Sua carta era à do ${cardPlayer.nome}</h2>`;
+      result.style.backgroundColor = "#d3bfbf52";
+      result.style.border = "solid 3px white";
+      result.style.borderRadius = "20px";
+      setInterval(function() {result.style.display = "none"},2000);
+    } else {
+      result.innerHTML = `<h1>Você empatou com ${cardPC.nome}</h1><h3>${cardPlayer.nome} VS ${cardPlayer.nome}</h3><img src="${cardPlayer.url}"><img src="${cardPC.url}"><h2>Sua carta era à do ${cardPlayer.nome}</h2>`;
+      result.style.backgroundColor = "#d3bfbf52";
+      result.style.border = "solid 3px white";
+      result.style.borderRadius = "20px";
+      setInterval(function() {result.style.display = "none"},2000);
     }
+    //console.log(cardPlayer.atributes[atribSelected]);
+    document.getElementById("btnSortear").disabled = false;
+  }
+}
